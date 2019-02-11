4a5d2513913958dfa3189373d0ab47a66e2ef526
Brian McGrath <brian.mcgrath@clavistechnology.com>
2019-02-11 14:18:12 +0000

    Updated font name

--- src/assets/scripts/components/Game/constants/font.js
+++ src/assets/scripts/components/Game/constants/font.js
@@ -1,11 +1,11 @@
 import { SCREEN } from './config';
 
-export const FONT_SIZES = {
-  SMALL: `${SCREEN.HEIGHT / 15}px font`,
-  MEDIUM: `${SCREEN.HEIGHT / 10}px font`,
-  LARGE: `${SCREEN.HEIGHT / 5}px font`,
-};
-
 export const FONT_TYPES = {
   MAIN: 'main_font',
+};
+
+export const FONT_SIZES = {
+  SMALL: `${SCREEN.HEIGHT / 15}px ${FONT_TYPES.MAIN}`,
+  MEDIUM: `${SCREEN.HEIGHT / 10}px ${FONT_TYPES.MAIN}`,
+  LARGE: `${SCREEN.HEIGHT / 5}px ${FONT_TYPES.MAIN}`,
 };
