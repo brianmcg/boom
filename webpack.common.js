4b5d5c77604b11befd0c1d5c2bd7b00bcb943f05
Brian McGrath <brian.mcgrath@clavistechnology.com>
2019-11-12 17:54:44 +0000

    Updated location of fonts

--- webpack.common.js
+++ webpack.common.js
@@ -102,10 +102,13 @@ module.exports = {
     new CopyWebpackPlugin([{
       from: path.resolve(__dirname, 'src/assets/manifest'),
       to: path.resolve(__dirname, 'dist/assets/manifest'),
-
+    }, {
+      from: path.resolve(__dirname, 'src/assets/fonts'),
+      to: path.resolve(__dirname, 'dist/assets/fonts'),
     }, {
       from: path.resolve(__dirname, 'src/assets/media'),
       to: path.resolve(__dirname, 'dist/assets/media'),
+
     }]),
     new MiniCssExtractPlugin({
       filename: 'assets/styles/[name].[chunkhash].min.css',
