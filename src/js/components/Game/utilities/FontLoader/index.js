ee76eacc977bb2ab7bb5b8c5102e0e1d3b9bd881
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2023-07-06 13:26:34 +0100

    Removed unneeded font loader

--- src/js/components/Game/utilities/FontLoader/index.js
+++
@@ -1,9 +0,0 @@
-export default class FontLoader {
-  static load(name) {
-    const fontFile = new FontFace(name, `url(./fonts/${name}.ttf)`);
-
-    document.fonts.add(fontFile);
-
-    return fontFile.load();
-  }
-}
